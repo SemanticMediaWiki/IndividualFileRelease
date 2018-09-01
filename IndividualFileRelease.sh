ceb0032f76e61f728978a874dd94f4ef6fc9d923
Karsten Hoffmeyer <karsten@hoffmeyer.info>
2018-09-01 14:36:04 +0200

    Provide initial version of shell script allowing to create an individual file release for Semantic MediaWiki.

---
+++ IndividualFileRelease.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# This shell script allows you to create an individual file release in case you
+# have no command line access to your webspace.
+
+# Variables
+installdirectory=/var/tmp
+softwaredirectory=mediawiki
+mediawiki=REL1_27
+semanticmediawiki=2.5.7
+semanticresultformats=2.5.5
+semanticcompoundqueries=1.1.0
+semanticextraspecialproperties=1.5.0
+
+# Commands
+echo
+echo "Creating individual file release for you."
+echo
+echo "Cloning in and checking out MediaWiki:"
+cd ${installdirectory}
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git ${softwaredirectory}
+cd ${softwaredirectory}
+git checkout origin/${mediawiki}
+echo "Done!"
+echo
+echo "Creating 'composer.local.json' file:"
+cat <<EOF >composer.local.json
+{
+	"require": {
+		"mediawiki/semantic-compound-queries": "${semanticcompoundqueries}",
+		"mediawiki/semantic-extra-special-properties": "${semanticextraspecialproperties}",
+		"mediawiki/semantic-media-wiki": "${semanticmediawiki}",
+		"mediawiki/semantic-result-formats": "${semanticresultformats}"
+	},
+	"config": {
+		"preferred-install": "source",
+		"optimize-autoloader": true
+	}
+}
+EOF
+echo "Done."
+echo
+echo "Installing MediaWiki dependencies as well as semantic extensions including dependencies:"
+composer update --no-dev --prefer-source
+echo "Done."
+echo
+echo "Cloning in and checking out Vector:"
+cd skins
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/skins/Vector.git
+cd Vector
+git checkout origin/${mediawiki}
+echo "Done!"
+echo
+echo "Removing '.git' directories of MediaWiki and Vector:"
+cd Vector
+rm -r .git/
+cd ../..
+rm -r .git/
+echo "Done."
+echo
+echo "The file release may now be moved to your webspace."
+echo
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # IndividualFileRelease
+
+The shell script allows you to create an individual file release in case you
+have no command line access to your webspace. It replaces the file releases
+that were previously done for Semantic MediaWiki 2.4.x to 2.5.x.
+
+This individual file release includes MediaWiki core, the Vector skin as well
+as the utmost essential semantic extensions Semantic MediaWiki, Semantic
+Result Formats, Semantic Extra Special Properties as well as Semantic Compound
+Queries.
+
+Note that you still have to adapt your wiki's "LocalSettings.php" file as well
+as create the required database tables.
